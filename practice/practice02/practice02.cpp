09458e01a0935bee53cf8b1f6debbef5ad9177d2
ArtSvi1 <tdbarnett1@ualr.edu>
2025-02-12 19:00:22 -0600

    Update practice02.cpp

--- practice/practice02/practice02.cpp
+++ practice/practice02/practice02.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string>
 
-double checkBalance()
+void checkFile()
+{
+    std::ifstream fin{ "account_balance.txt" };
+    if (fin)
+    {
+        std::string balance;
+        fin >> balance;
+        std::cout << "Your current balance is: " << balance << std::endl;
+    }
+    else
+    {
+        fin.close();
+        std::ofstream fout{ "account_balance.txt" };
+        if (!fout)
+        {
+            std::cerr << "File failed to open.\n";
+            return;
+        }
+        else
+        {
+            fout << "$100.00";
+            std::cout << "Initializing account with $100.00...\n" << std::endl;
+            fout.close();
+        }
+    }
+}
+
+void checkBalance()
 {
     std::ifstream fin{ "account_balance.txt" };
     if (!fin)
@@ -12,23 +40,71 @@ double checkBalance()
     }
     std::string balance;
     fin >> balance;
+    fin.close();
 
     std::cout << "Your current balance is: " << balance << std::endl;
 }
 
-void withdraw()
+void withdraw(double with)
 {
-
+    std::ifstream fin{ "account_balance.txt" };
+    if (!fin)
+    {
+        std::cerr << "File failed to open.";
+        return;
+    }
+    std::string b;
+    fin >> b;
+    fin.close();
+    double balance = 0;
+    balance = std::stod(&b[1]);
+    if (with > balance)
+    {
+        std::cout << "Insufficient funds. Your balance is: " << b << std::endl;
+    }
+    else if (with <= 0) 
+    {
+        std::cout << "Withdrawal must be positive." << std::endl;
+    }
+    else
+    {
+        double newBalance = balance - with;
+        std::ofstream fout{ "account_balance.txt" };
+        fout << "$" << std::fixed << std::setprecision(2) << newBalance;
+        fout.close();
+        std::cout << "Withdrawal successful. Your new balance is: $" << std::fixed << std::setprecision(2) << newBalance << std::endl;
+    }
 }
 
-void deposit()
+void deposit(double depo)
 {
+    std::ifstream fin{ "account_balance.txt" };
+    if (!fin)
+    {
+        std::cerr << "File failed to open.";
+        return;
+    }
+    std::string b;
+    fin >> b;
+    fin.close();
+    double balance = 0;
+    balance = std::stod(&b[1]);
 
+    if (depo <= 0)
+    {
+        std::cout << "Deposit must be positive." << std::endl;
+    }
+    else
+    {
+        double newBalance = balance + depo;
+        std::ofstream fout{ "account_balance.txt" };
+        fout << "$" << std::fixed << std::setprecision(2) << newBalance;
+        fout.close();
+        std::cout << "Deposit successful. Your new balance is: $" << std::fixed << std::setprecision(2) << newBalance << std::endl;
+    }
 }
 
 int main()
 {
-    std::ofstream fout{ "account_balance.txt" };
-    fout << "$100.00";
     return 0;
 }
