44a6f6f749a3fb06994437fe82c401ffc07702b5
ArtSvi1 <tdbarnett1@ualr.edu>
2025-02-17 16:14:15 -0600

    First Commit
    Initial Setup for Practice03

---
+++ practice/practice03/fixes.md
@@ -0,0 +1,1 @@
+Documented Bugs and Fixes:
---
+++ tests/practice03_test/practice03_test/practice03_test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../practice/practice03/practice03.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace practice03test
+{
+	TEST_CLASS(practice03test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
